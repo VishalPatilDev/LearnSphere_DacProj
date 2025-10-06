0f43225fe72a230e3cc653aa80f2a396e41d3d8a
Vishal Patil <vishalpatil.dev2@gmail.com>
2025-10-07 00:12:24 +0530

    Delete Cdac_Project directory

--- Cdac_Project/frontend/src/Components/ErrorPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function ErrorPage() {
-  return (
-    <div style={{paddingTop:'50px'}}><h2 style={{color:'red'}}>Something Went Wrong...</h2></div>
-  )
-}
-
-export default ErrorPage;
